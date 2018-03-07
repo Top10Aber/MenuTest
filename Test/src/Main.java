3f6660a56ff71dcd2d9d00ec66c735d228e5470c
Tommy Pugh <top10@aber.ac.uk>
2018-03-07 10:21:54 +0000

    Initial commit

---
+++ Test/src/Main.java
@@ -0,0 +1,67 @@
+import java.awt.event.*;
+import javax.swing.*;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		JFrame frame = new JFrame();
+		JPanel panel = new JPanel();
+		panel.setLayout(null);
+		JPasswordField passwordField = new JPasswordField();
+		JButton button = new JButton("Submit");
+		JLabel label = new JLabel("");
+		JLabel label2 = new JLabel("Enter assessor password");
+		JLabel label3 = new JLabel("");
+		frame.setAlwaysOnTop(true);
+
+		button.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				String get = "r";
+				@SuppressWarnings("deprecation")
+				String test = passwordField.getText();
+
+				if (get.equals(test)) {
+
+					label.setText("Confirmed");
+					label3.setText("");	
+
+					Timer timer = new Timer(2000, new ActionListener() {
+	                    @Override
+	                    public void actionPerformed(ActionEvent e) {
+	                        frame.dispose();
+	                    }
+	                });
+	                timer.start();
+					
+
+				} else {
+					label3.setText("Invalid password");
+					label.setText("");
+
+				}
+
+			}
+		});
+
+		
+		
+		panel.add(passwordField);
+		passwordField.setBounds(5, 30, 70, 20);
+		panel.add(button);
+		button.setBounds(80, 30, 75, 20);
+		panel.add(label);
+		label.setBounds(50, 55, 100, 20);
+		panel.add(label2);
+		label2.setBounds(7, 5, 500, 20);
+		panel.add(label3);
+		label3.setBounds(30, 55, 100, 20);
+		frame.setSize(177, 120);
+		frame.setLocationRelativeTo(null);
+		//frame.setUndecorated(true);
+		//frame.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
+		frame.getContentPane().add(panel);
+		frame.setVisible(true);
+				
+	}
+}
